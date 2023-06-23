6d333b1cd42e313da9050c97eb76bf019b3b7b21
DimaMakarenko <makarenko.d.viri@gmail.com>
2023-06-23 10:50:28 +0300

    get data by useSWR

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,20 +1,21 @@
 'use client';
 
 import Link from "next/link";
-import { usePosts } from "@/store";
-import { useEffect, useState } from "react";
-import { shallow } from 'zustand/shallow';
+import  useSWR  from "swr";
+import { getAllPosts } from "@/services/getPosts";
 
 const Posts = () => {
-  const [posts, loading, getAllPosts] = usePosts(state => [state.posts, state.loading, state.getAllPosts], shallow)
+  // const [posts, loading, getAllPosts] = usePosts(state => [state.posts, state.loading, state.getAllPosts], shallow)
 
-  useEffect(() => {
-    getAllPosts()
-  }, [getAllPosts]);
+  // useEffect(() => {
+  //   getAllPosts()
+  // }, [getAllPosts]);
+
+  const {data:posts, isLoading} = useSWR('posts', getAllPosts)
 
   return (
     <>
-      {loading ? <h3>Loading...</h3> : <ul>
+      {isLoading ? <h3>Loading...</h3> : <ul>
         {posts.map((post: any) => <li key={post.id}>
           <Link href={`/blog/${post.id}`}>{post.title}</Link>
         </li>
